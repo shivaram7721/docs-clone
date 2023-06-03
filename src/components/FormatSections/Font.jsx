fcf7c4a5b6bf1d411d0104074a845aa18c6d9098
shivaram7721 <shivaramdusa7721@gmail.com>
2023-06-03 19:49:07 +0530

    changed fontfamily

--- src/components/FormatSections/Font.jsx
+++ src/components/FormatSections/Font.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export function FontFamily() {
+export function Font() {
   const [selectedOption, setSelectedOption] = useState('');
 
   function handleFamily(e) {
